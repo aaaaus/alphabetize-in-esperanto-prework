216c8aefacca027eeb93b39ce0f0c7e2b2b82090
Austin Luft <ausluft@gmail.com>
2018-10-04 22:43:40 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,7 +6,7 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
   arr.sort_by do | sentence |
-    sentence.split("").each do | character |
+    sentence.split("").collect do | character |
       ESPERANTO_ALPHABET.index(character)
     end
   end
